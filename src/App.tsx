19edba657dafb8265a27a1bb5cb6770ce8bc3596
Artsiom Bahrantsau <artsiom.bahrantsau@innowise.com>
2025-04-15 14:41:34 +0200

    chore: added gtm custom event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  useEffect(()=>{
+    if (!count) return 
+    //@ts-ignore
+    window.dataLayer.push({
+      event: 'increment',
+      incrementValue: count
+    })
+  },[count])
   return (
     <>
       <div>
